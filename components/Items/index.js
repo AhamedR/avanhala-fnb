3063bc7b114153013d1489b7ed3f099149c4969c
Ahamed Rasheed <ahamed.rasheed@akqa.com>
2023-03-16 01:02:19 +0530

    new version

--- components/Items/index.js
+++ components/Items/index.js
@@ -22,12 +22,14 @@ const Items = () => {
           return (
             <div id={`${item.title.replace(/ /g,'-')}`} key={item.title} className={styles.container}>
               <h2 className={styles.title}>{item.title}</h2>
+              <p className={styles.description}>{item.description}</p>
               <ul className={styles.allItems}>
                 {
                   items.map(variation => 
                     <li key={variation.id} className={styles.item}>
                       <p className={styles.number}>{variation.id}</p>
                       <h3 className={styles.name}>{variation.name}</h3>
+                      <p className={styles.description}>{variation.description}</p>
                       <p className={styles.price}>{variation.price}.00</p>
                     </li>
                   )
